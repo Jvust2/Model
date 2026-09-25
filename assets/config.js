c601c54e0bfca01215df9ab5239de2761183b30b
Jvust2 <1433223jb78@gmail.com>
2026-09-25 11:24:29 +0800

    feat: add Drive model runtime config

---
+++ assets/config.js
@@ -0,0 +1,11 @@
+(function () {
+  "use strict";
+
+  window.MODEL_CONFIG = Object.freeze({
+    oauthBridge: "https://drive-oauth-bridge.143322378jb.workers.dev",
+    runtimeBase: "http://127.0.0.1:8765",
+    maxFolders: 2000,
+    maxModelFiles: 20000,
+    rangeProbeBytes: 4096
+  });
+})();
